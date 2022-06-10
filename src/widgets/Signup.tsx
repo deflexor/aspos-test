af2ab55b332e5e23ba5bfb69996213425e269aa0
Dmitriy Knyazev <deflexor@gmail.com>
2022-06-10 05:14:43 +0300

    ...

---
+++ src/widgets/Signup.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Signup() {
+    return (<div className="p-3 bg-warning rounded">
+        <p className="mb-0">Sign up Widget.</p>
+        </div>
+    );
+}
